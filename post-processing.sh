94e1d859f4988153b9ddd47d26d9226271bc23d4
KimHe <kingdomhe@163.com>
2019-04-30 18:47:41 +0800

    [feature]: take the maximal one when repetition occurs

--- post-processing.sh
+++ post-processing.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Maintained by Qiao-Le He
 
+
 for STRING in `cat strings`
 do
     for VARIABLE in `cat files`
     do
-        egrep -i $STRING $VARIABLE.csv | awk '{print $3}' > $VARIABLE
+        egrep -i $STRING $VARIABLE.csv | awk '{print $3}' | sort -n | tail -1 > $VARIABLE
     done
     paste `cat files` > $STRING
     rm `cat files`
@@ -18,12 +19,12 @@ rm tmp temp `cat strings`
 
 for STRING in `cat strings`
 do
-    for VARIABLE in `cat files`
-    do
-        egrep -i $STRING $VARIABLE.csv | awk '{print $4}' > $VARIABLE
-    done
-    paste `cat files` > $STRING
-    rm `cat files`
+   for VARIABLE in `cat files`
+   do
+       egrep -i $STRING $VARIABLE.csv | awk '{print $4}' | sort -n | tail -1 > $VARIABLE
+   done
+   paste `cat files` > $STRING
+   rm `cat files`
 done
 
 cat `cat strings` > tmp
