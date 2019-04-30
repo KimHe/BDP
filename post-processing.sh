f49c61cdb401a6c35df4d4b256a3c47ab3616dff
KimHe <kingdomhe@163.com>
2019-04-30 11:31:09 +0800

    [bug]: store Area and Relative content information respectively

--- post-processing.sh
+++ post-processing.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # Maintained by Qiao-Le He
-# 
-
-firstRow=NC01
 
 for STRING in `cat strings`
 do
     for VARIABLE in `cat files`
     do
-        egrep -i $STRING $VARIABLE.csv | awk '{print $4, $3}' > $VARIABLE
+        egrep -i $STRING $VARIABLE.csv | awk '{print $3}' > $VARIABLE
     done
     paste `cat files` > $STRING
     rm `cat files`
@@ -16,6 +13,20 @@ done
 
 cat `cat strings` > tmp
 paste strings tmp > temp
-cat files temp > output
+cat files temp > BDP_Area.out
 rm tmp temp `cat strings`
 
+for STRING in `cat strings`
+do
+    for VARIABLE in `cat files`
+    do
+        egrep -i $STRING $VARIABLE.csv | awk '{print $4}' > $VARIABLE
+    done
+    paste `cat files` > $STRING
+    rm `cat files`
+done
+
+cat `cat strings` > tmp
+paste strings tmp > temp
+cat files temp > BDP_Relative.out
+rm tmp temp `cat strings`
