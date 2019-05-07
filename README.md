a232001a6a7ef58c02cfd3c0166c02296e26e587
KimHe <kingdomhe@163.com>
2019-05-07 17:34:45 +0800

    [feature]: add more options
            1. automatically generate strings for the program
            2. check the name pattern of the genetrated strings
            3. support both CAS no and element name search
            4. claim the guide rules for the pattern names
            5. update the program in C language (converted from shc)

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # BASH-based data post-processing (BDP)
 
 * files: the file names of data that you are going to process
-* strings: the *CAS No.* of elements of data that you want to extract from each data file
+* strings: the *CAS No.* or *names* of elements of data that you want to extract from each data file
 
+GUIDLINE:
+
+For the csv datasets:
+0. In all csv files, PLEASE delete the HEAD rows 
+    (these would be redundant information for the program);
+1. PLEASE put the element names at the END column
+    (because element names have varying lengths);
+
+For the output datasets
+0. All spaces were replaced with \s;
+1. All square brackets [] were replaced with \[\];
+2. The initial dots (.) in the element names were deleted;
+3. All asterisks were replaced with dots (.);
+4. All primes in the element names might be dangerous for the program
--- bdp.sh
+++ bdp.sh
@@ -5,64 +5,125 @@ echo "                :-) BASH-based data post-processing (BDP) (-:"
 echo " "
 echo "                       BDP is written by Qiao-Le He"
 echo " "
-echo "          copyright (c) 2019, East China University of Science and"
-echo "           Technology, Shanghai, China"
+echo -e "\t copyright (c) 2019, East China University of Science and"
+echo -e "\t  Technology, 200237 Shanghai, China"
 echo " "
-echo "          check out https://github.com/KimHe/BDP for more information"
+echo -e "\t check out https://github.com/KimHe/BDP for more information"
 echo " "
-echo "          BDP is free software; you can redistribute it and/or modify"
-echo "          it under the terms of the GNU Lesser General Public License"
+echo -e "\t BDP is free software; you can redistribute it and/or modify"
+echo -e "\t it under the terms of the GNU Lesser General Public License"
 echo " "
 
 
-if [ ! -z "$1" ];
+while [[ "$#" -gt 0 ]];
+do
+    case $1 in
+        -h|--help) help=1; shift;;
+        -s|--sort) sort=1;;
+        -g|--generate) generate=1;;
+        -c|--check) check=1;;
+        *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift;
+done
+
+if [ "$help" == 1 ];
 then
-    if [ "$1" == "--help" ] || [ "$1" == "-h" ]
-    then
-        echo "    Execute 'bash bdp.sh -h' for more help information"
-        echo "    Execute 'bash bdp.sh -s' to sort strings w.r.t. CAS #"
-        echo " "
-        exit 0
-    fi
-    if [ $1 = '--sort' ] || [ $1 = '-s' ];
-    then
-        sort -n strings | tee strings_sorted | mv strings_sorted strings
-    fi
+    echo "[SYNOPSIS]:"
+    echo -e "\t bash bdp.sh -h -g -s -c"
+    echo ""
+    echo "[OPTIONS]:"
+    echo -e "\t -h, --help"
+    echo -e "\t \t Display help information and exit"
+    echo -e "\t -g, --generate"
+    echo -e "\t \t Generate the STRING file from the raw data"
+    echo -e "\t -s, --sort"
+    echo -e "\t \t Strings are numerically or alphabetically sorted"
+    echo -e "\t -c, --check"
+    echo -e "\t \t Check whether the STRINGS pattern is qualified for this program. Square brackets [] will be replaced with \[\]. Trailing spaces will be omitted. Spaces between names will be replaced with \s."
+    echo " "
+    exit 0
+fi
+
+# Generate STRINGS from the raw datasets
+if [ "$generate" == 1 ];
+then
+    for VARIABLE in `cat files`
+    do
+        cat $VARIABLE.csv | awk '{print $7, $8, $9, $10, $11, $12, $13}' | sort -n > $VARIABLE
+    done
+    cat `cat files` > strings
+    rm `cat files`
+    echo "[INFO]: The STRINGS file has been generated from the raw data"
+    echo ""
+fi
+
+# Sort element names in the STRING
+if [ "$sort" == 1 ];
+then
+    sort -n strings > strings_sorted
+    sort -u strings_sorted > strings_unique
+    mv strings_unique strings | rm strings_sorted
+    echo "[INFO]: The elements in the STRING have been sorted"
+    echo ""
+fi
+
+cp strings .strings
+
+# Check the pattern of element names in the STRING
+if [ "$check" == 1 ];
+then
+    # Trailing
+    sed -i 's/\s\+$//g' .strings
+    # Suppress file names start with .
+    sed -i 's/^\.//g'   .strings
+    # Replace square brackets
+    sed -i 's/\[/\\[/g' .strings
+    sed -i 's/\]/\\]/g' .strings
+    # Replace spaces of file names with \s
+    sed -i 's/ /\\s/g'  .strings
+    # Replace asterisks with dot
+    sed -i 's/*/./g' .strings
+    echo "[INFO]: The pattern of STRINGS has been checked and modified"
+    echo ""
 fi
 
 
-
-for STRING in `cat strings`
+# Extract Area information
+for STRING in `cat .strings`
 do
     for VARIABLE in `cat files`
     do
-        egrep -i $STRING $VARIABLE.csv | awk '{print $3}' | sort -n | tail -1 > $VARIABLE
+        grep -i -w $STRING $VARIABLE.csv | awk '{print $3}' | sort -n | tail -1 > $VARIABLE
     done
     paste `cat files` > $STRING
     rm `cat files`
     echo -n "."
 done
-
-cat `cat strings` > tmp
-paste strings tmp > temp
-cat files temp > BDP_Area.out
-rm tmp temp `cat strings`
-
-for STRING in `cat strings`
-do
-    for VARIABLE in `cat files`
-    do
-        egrep -i $STRING $VARIABLE.csv | awk '{print $4}' | sort -n | tail -1 > $VARIABLE
-    done
-    paste `cat files` > $STRING
-    rm `cat files`
-    echo -n "."
-done
-
 echo ""
-cat `cat strings` > tmp
-paste strings tmp > temp
-cat files temp > BDP_Relative.out
-rm tmp temp `cat strings`
 
-echo "DONE"
+cat `cat .strings` > tmp
+paste .strings tmp > temp
+cat files temp > BDP_Area.out
+rm tmp temp `cat .strings`
+
+
+# Extract Relative information
+for STRING in `cat .strings`
+do
+    for VARIABLE in `cat files`
+    do
+        grep -i -w $STRING $VARIABLE.csv | awk '{print $4}' | sort -n | tail -1 > $VARIABLE
+    done
+    paste `cat files` > $STRING
+    rm `cat files`
+    echo -n "."
+done
+echo ""
+
+cat `cat .strings` > tmp
+paste .strings tmp > temp
+cat files temp > BDP_Relative.out
+rm tmp temp `cat .strings`
+
+echo "[INFO]: DONE"
+
